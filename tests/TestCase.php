e7835636b8e7b45c7167f4fe71390e0e36e50df1
Peter Sall <pesa0015@gmail.com>
2018-02-24 15:55:25 +0000

    Add test setup with migration and seed to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
+    }
 }
