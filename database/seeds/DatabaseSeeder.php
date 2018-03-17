4b0c720956403c632dbbc9a0ba2c44b7508237db
Peter Sall <pesa0015@gmail.com>
2018-03-17 18:27:19 +0000

    Output warning and return null if seed file doesn't exist

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -34,6 +34,11 @@ class DatabaseSeeder extends Seeder
         
         $file = $this->databasePath . '/seeds/data/' . $fileName;
 
+        if (!File::exists($file)) {
+            $this->command->warn('File ' . $fileName . ' does not exist, couldn\'t seed');
+            return null;
+        }
+
         $json = json_decode(file_get_contents($file));
 
         return $json;
