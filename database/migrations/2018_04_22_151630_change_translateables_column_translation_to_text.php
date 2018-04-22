7eedadf05637a6a272650b6e41358760d84bf94a
Peter Sall <pesa0015@gmail.com>
2018-04-22 15:38:07 +0000

    Remove down() method in translateables column change due to string will never work

--- database/migrations/2018_04_22_151630_change_translateables_column_translation_to_text.php
+++ database/migrations/2018_04_22_151630_change_translateables_column_translation_to_text.php
@@ -25,8 +25,6 @@ class ChangeTranslateablesColumnTranslationToText extends Migration
      */
     public function down()
     {
-        Schema::table('translateables', function (Blueprint $table) {
-            $table->string('translation')->change();
-        });
+        //
     }
 }
