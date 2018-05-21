bfc6b7a3f85193579572f71edfafcb05a45460e2
Peter Sall <pesa0015@gmail.com>
2018-05-21 20:32:46 +0000

    Remove unused imports in App\Mail\Contact and add Request to constructor

--- app/Mail/Contact.php
+++ app/Mail/Contact.php
@@ -2,23 +2,21 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
 
 class Contact extends Mailable
 {
-    use Queueable, SerializesModels;
-
+    private $request;
+    
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Request $request)
     {
-        //
+        $this->request = $request;
     }
 
     /**
