4d3b938669b5f254417f47288d8f2e30bb905878
Peter Sall <pesa0015@gmail.com>
2018-05-21 20:17:30 +0000

    Move imports in ContactsController to method

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ContactsRequest;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\Contact;
-use App\Mail\ContactCopy;
 
 class ContactsController extends Controller
 {
-    public function contactUs(ContactsRequest $request)
+    public function contactUs(\App\Http\Requests\ContactsRequest $request)
     {
-        Mail::to(env('MAIL_TO'))->send(new Contact($request));
+        Mail::to(env('MAIL_TO'))->send(new \App\Mail\Contact($request));
 
-        Mail::to($request->email)->send(new ContactCopy($request));
+        Mail::to($request->email)->send(new \App\Mail\ContactCopy($request));
 
         return response()->json([], 200);
     }
