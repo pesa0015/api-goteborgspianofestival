83ec942641f4795bdb2b19d2d8416f8ead2598ac
Peter Sall <pesa0015@gmail.com>
2018-06-02 20:49:03 +0000

    Fix typo Membershop => Membership

--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,5 @@ Route::resource('applications', 'ApplicationsController', ['only' => ['store']])
 // Contact
 Route::post('contact', 'ContactsController@contactUs');
 
-// Membershop
+// Membership
 Route::post('member', 'ContactsController@beMember');
